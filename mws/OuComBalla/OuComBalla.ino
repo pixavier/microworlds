04edaa47cd2047525a51f98244dd44b1404e6546
Xavier Pi <63783572+pixavier@users.noreply.github.com>
2023-12-19 02:58:30 +0100

    Add files via upload

--- mws/OuComBalla/OuComBalla.ino
+++ mws/OuComBalla/OuComBalla.ino
@@ -1,77 +1,146 @@
-/*
- Basic MQTT example
+#include <WiFi.h>
+#include "PubSubClient.h"
+#include <analogWrite.h>
 
- This sketch demonstrates the basic capabilities of the library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic"
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
+const char* ssid = "iotcat";
+const char* password = "1234567890a";
+const char* mqttServer = "broker.emqx.io";
+int port = 1883;
+String stMac;
+char mac[50];
+char clientId[50];
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- 
-*/
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-#include <SPI.h>
-#include <Ethernet.h>
-#include <PubSubClient.h>
+int onPeriod = 0;
+unsigned long t = 0;
+int pwm = 0;
 
-// Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(172, 16, 0, 100);
-IPAddress server(172, 16, 0, 2);
+void setup() {
+  Serial.begin(115200);
+  randomSeed(analogRead(0));
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
-  }
+  delay(10);
   Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  wifiConnect();
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
+  stMac = WiFi.macAddress();
+  stMac.replace(":", "_");
+  Serial.println(stMac);
+  
+  client.setServer(mqttServer, port);
+  client.setCallback(callback);
+
+  pinMode(22, INPUT_PULLUP);
+  pinMode(23, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  analogWriteResolution(10);
+  analogWriteFrequency(500);  
 }
 
-EthernetClient ethClient;
-PubSubClient client(ethClient);
+void wifiConnect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
 
-void reconnect() {
-  // Loop until we're reconnected
+void mqttReconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("arduinoClient")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic","hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
+    long r = random(1000);
+    sprintf(clientId, "clientId-%ld", r);
+    if (client.connect(clientId)) {
+      Serial.print(clientId);
+      Serial.println(" connected");
+      client.subscribe("name/up");
+      client.subscribe("name/down");
+      client.subscribe("name/pwm");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
-void setup()
-{
-  Serial.begin(57600);
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String stPayload;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char) payload[i]);
+    stPayload += (char) payload[i];
+  }
 
-  client.setServer(server, 1883);
-  client.setCallback(callback);
-
-  Ethernet.begin(mac, ip);
-  // Allow the hardware to sort itself out
-  delay(1500);
+  String stTopic = String(topic);
+  int val = stPayload.toInt();
+  
+  if (stTopic == "name/up") {
+    onPeriod += val;
+  } else if (stTopic == "name/down"){
+    onPeriod -= val;
+  } else if (stTopic == "name/pwm") {
+    onPeriod = val;
+  }
 }
 
-void loop()
-{
+void loop() {
+  delay(10);
   if (!client.connected()) {
-    reconnect();
+    mqttReconnect();
   }
   client.loop();
+
+  int i22 = digitalRead(22);
+  int i23 = digitalRead(23);
+/*  
+  Serial.println(i4);
+  if (i4 == 0) {
+    digitalWrite(26, HIGH);
+  } else {
+    digitalWrite(26, LOW); 
+  }
+*/
+
+  if (i22 == 0) {
+    onPeriod -= 10;
+  }
+  
+  if (i23 == 0) {
+    onPeriod += 10;
+  }
+
+  if (onPeriod < 0) onPeriod = 0;
+  if (onPeriod > 1000) onPeriod = 1000;
+
+  t = millis() % 1000;
+  pwm = 1023 * onPeriod / 1000;
+
+  analogWrite(13, pwm);    
+
+  /*
+  if (t < onPeriod) {
+    analogWrite(13, pwm);    
+  } else {
+    analogWrite(13, 0);
+  } 
+  */
+    
+  Serial.println(onPeriod);
+  delay(40);
 }
